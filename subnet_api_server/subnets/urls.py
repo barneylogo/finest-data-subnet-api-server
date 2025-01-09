e0e451f8559858f49a0bd0bb0b23ea241a513b0f
barneylogo <ben.amwithyou@gmail.com>
2025-01-09 16:07:00 +0000

    feat: check-task api endpoint for validator

--- subnet_api_server/subnets/urls.py
+++ subnet_api_server/subnets/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from .views import CheckTaskViewSet
 from .views import FinishTaskViewSet
 from .views import GetTaskViewSet
 
 urlpatterns = [
     path("get-task/", GetTaskViewSet.as_view()),
     path("finish-task/", FinishTaskViewSet.as_view()),
+    path("check-task/", CheckTaskViewSet.as_view()),
 ]
