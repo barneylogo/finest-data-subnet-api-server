1b5c41affc2366ff99f522e93151228e63e0e68e
tobiashomie <tobiashomie@gmail.com>
2025-01-10 06:38:31 -0700

    fix: refine brand

--- config/settings/base.py
+++ config/settings/base.py
@@ -230,7 +230,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Ben""", "ben@cerebromesh.io")]
+ADMINS = [("""Tobias""", "tobias@elevonix.io")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 # https://cookiecutter-django.readthedocs.io/en/latest/settings.html#other-environment-settings
--- config/settings/production.py
+++ config/settings/production.py
@@ -18,7 +18,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["cerebromesh.io"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["elevonix.io"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -122,7 +122,7 @@ STATIC_URL = f"https://{aws_s3_domain}/static/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="subnet-api-server <noreply@cerebromesh.io>",
+    default="subnet-api-server <noreply@elevonix.io>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
