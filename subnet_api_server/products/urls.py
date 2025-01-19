16cda9ce2ebdae12476e8c201b4797e95fa50741
tobiashomie <tobiashomie@gmail.com>
2025-01-19 01:57:16 -0700

    chore: default value for count in products endpoint

--- subnet_api_server/products/urls.py
+++ subnet_api_server/products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from subnet_api_server.products.views import GetProductsView
 
 urlpatterns = [
-    path("<int:count>/", GetProductsView.as_view()),
+    path("<int:count>/", GetProductsView.as_view(), kwargs={"count": 0}),
 ]
