81b4f8d692bcfee5d46f22920dbd82d7e7d9478c
barneylogo <ben.amwithyou@gmail.com>
2025-01-21 09:08:51 +0000

    feat: Add task for making 1 days passed task as failed

--- config/settings/base.py
+++ config/settings/base.py
@@ -5,6 +5,7 @@ import ssl
 from pathlib import Path
 
 import environ
+from celery.schedules import crontab
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # subnet_api_server/
@@ -303,6 +304,15 @@ CELERY_TASK_TIME_LIMIT = 5 * 60
 CELERY_TASK_SOFT_TIME_LIMIT = 60
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#beat-scheduler
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+
+CELERY_BEAT_SCHEDULE = {
+    "mark-stale-tasks-as-failed": {
+        "task": "subnet_api_server.subnets.tasks.mark_stale_tasks_as_failed",
+        "schedule": crontab(minute="*/30"),
+    },
+}
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker-send-task-events
 CELERY_WORKER_SEND_TASK_EVENTS = True
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-task_send_sent_event
