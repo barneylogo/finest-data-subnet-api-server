7037304f897fa5302fd5f13018bbeb188ccdc1c7
tobiashomie <tobiashomie@gmail.com>
2025-01-27 07:08:23 -0700

    chore: filter completed tasks only

--- subnet_api_server/stats/urls.py
+++ subnet_api_server/stats/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from subnet_api_server.stats.views import GetValidatorsView, GetWeightsView
+from subnet_api_server.stats.views import GetValidatorsView, GetScoresView
 
 urlpatterns = [
     path("validators/", GetValidatorsView.as_view()),
-    path("weights/", GetWeightsView.as_view()),
+    path("scores/", GetScoresView.as_view()),
 ]
